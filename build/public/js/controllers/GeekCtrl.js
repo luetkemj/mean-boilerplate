6121e1ebd9e760cd39f4b29eaf274cced8fb3d84
Mark Luetke <luetkemj@gmail.com>
2015-07-26 22:47:41 -0400

    delete build

--- build/public/js/controllers/GeekCtrl.js
+++
@@ -1,6 +0,0 @@
-angular.module('GeekCtrl', []).controller('GeekController', function($scope) {
-
-	$scope.tagline = 'The square root of life is pi!';
-	$scope.butter = 'butter is about the very best thing ever in teh hostory of ever';	
-
-});
